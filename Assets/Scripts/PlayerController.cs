09615f5fa6b76ae4df1c95dabee7df211a63cf3d
ChristopherHubbard <hubbard8@purdue.edu>
2017-06-01 15:21:58 -0400

    More Consistent Jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     //Public variables to manipulate in Unity
     public float speed;
-    public int jumpHeight;
+    public int jumpVelocity;
+    public bool collCheck = false;
 
     //Private variables for movement
     private Rigidbody2D rigidB;
@@ -19,22 +20,22 @@ public class PlayerController : MonoBehaviour
     }
 
     //Called in a fixed interval -- best used when dealing with Physics
-    private void FixedUpdate()
+    private void Update()
     {
         //Get the input for the horizontal movement
         float moveHorizontal = Input.GetAxis("Horizontal");
-        //Default is not jumping
-        float jump = 0;
+
         //If the space bar is pushed and the Shape Shifter isn't already jumping
         if (Input.GetKeyDown("space") && !isJumping)
         {
             //Set isJumping to true and set jump to the jumpHeight 
             isJumping = true;
-            jump = jumpHeight;
+            rigidB.velocity = new Vector2(rigidB.velocity.x, jumpVelocity);
         }
         //Create a new Vector2 with the given force directions -- add the force to the rigid body
-        Vector2 movement = new Vector2(moveHorizontal, jump);
-        rigidB.AddForce(movement * speed);
+        //Vector2 movement = new Vector2(moveHorizontal, 0);
+        //rigidB.AddForce(movement * speed);
+        rigidB.velocity = new Vector2(moveHorizontal * speed, rigidB.velocity.y);
 
     }
 
@@ -46,11 +47,27 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionStay2D(Collision2D collision)
     {
         //Collision check to see if the player is on the top of the collision object
-        bool collCheck = collision.gameObject.transform.position.y <= transform.position.y;
+        //collCheck = collision.gameObject.transform.position.y + (collision.gameObject.transform.lossyScale.y / 2) <= transform.position.y - (transform.lossyScale.y / 2);
+        collCheck = IsGrounded();
         //If the player collided with the floor
-        if(collision.gameObject.tag == "Floor" && isJumping && collCheck)
+        if(collision.gameObject.tag == "Floor" && collCheck)
         {
             isJumping = false;
         }
+        /*
+        else if(collision.gameObject.tag == "Floor" && !collCheck)
+        {
+            rigidB.velocity = new Vector2(0, rigidB.velocity.y);
+        }
+        */
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, -Vector2.up, GetComponent<Collider2D>().bounds.extents.y + 0.1f);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isJumping = true;
     }
 }
