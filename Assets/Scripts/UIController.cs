3d44e399f2c9cc748a1342385eba040e0a1ed321
ChristopherHubbard <hubbard8@purdue.edu>
2017-07-05 23:31:37 -0400

    Level 1-5 Began

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UIController : MonoBehaviour
+public class UIController: MonoBehaviour
 {
     public List<GameObject> UIObjects { get; set; }
     public AudioSource Music { get; set; }
